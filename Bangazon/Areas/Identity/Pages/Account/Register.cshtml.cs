7fcef08d68aa0ebbb8969afa298215aa3ea8d0ee
Your Name <you@example.com>
2019-12-03 14:31:27 -0600

    Able to create an order when needed and add products

--- Bangazon/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Bangazon/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -83,23 +83,12 @@ namespace Bangazon.Areas.Identity.Pages.Account
                     FirstName = Input.FirstName,
                     LastName = Input.LastName,
                     StreetAddress = Input.StreetAddress,
-                    UserName = Input.Email, Email = Input.Email
+                    UserName = Input.Email, Email = Input.Email,
+                    EmailConfirmed = true
                 };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
-                    _logger.LogInformation("User created a new account with password.");
-
-                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                    var callbackUrl = Url.Page(
-                        "/Account/ConfirmEmail",
-                        pageHandler: null,
-                        values: new { userId = user.Id, code = code },
-                        protocol: Request.Scheme);
-
-                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
-
                     await _signInManager.SignInAsync(user, isPersistent: false);
                     return LocalRedirect(returnUrl);
                 }
